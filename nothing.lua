a090f12d74bf1f3d33014bc44c90e4066d320a83
lolpoppyus <63702146+lolpoppyus@users.noreply.github.com>
2025-02-15 00:48:44 -0600

    Update nothing.lua

--- nothing.lua
+++ nothing.lua
@@ -34,12 +34,12 @@ local counter = 0
 local counterLabel = AutoUnits:Label({Text = "Replayed: 0"},"CounterLabel")
 
 local placeUnits = {
-    ["AiHoshinoEvo"] = {Placed = false, Cost = 600},
-    ["Gogeta"] = {Placed = false, Cost = 3850},
-    ["Giorno_GER"] = {Placed = false, Cost = 750},
-    ["EscanorGodly"] = {Placed = false, Cost = 3750},
-    ["DioOHShiny"] = {Placed = false, Cost = 3500},
-    ["ChaeHae"] = {Placed = false, Cost = 1100}
+    ["AiHoshinoEvo"] = {Placed = false,Position = CFrame.new(76, 0, -209), Cost = 600},
+    ["Giorno_GER"] = {Placed = false,Position = CFrame.new(90, 0, -215), Cost = 750},
+    ["ChaeHae"] = {Placed = false,Position = CFrame.new(81, 0, -188), Cost = 1100},
+    ["EscanorGodly"] = {Placed = false,Position = CFrame.new(106, 0, -223), Cost = 3750},
+    ["DioOHShiny"] = {Placed = false,Position = CFrame.new(90, 0, -222), Cost = 3500},
+    ["Gogeta"] = {Placed = false,Position = CFrame.new(79, 0, -226), Cost = 3850}
 }
 
 local allPlaced = false
@@ -119,32 +119,32 @@ RunService.RenderStepped:Connect(function()
         local cash = Player:WaitForChild("Cash").Value
         if placeUnits["AiHoshinoEvo"].Placed == false then
             if cash >= placeUnits["AiHoshinoEvo"].Cost then
-                PlaceUnit("AiHoshinoEvo",CFrame.new(-633.967529296875, -57.04720687866211, -376.8282470703125))
+                PlaceUnit("AiHoshinoEvo",placeUnits["AiHoshinoEvo"].Position)
             end
         else
             if placeUnits["Giorno_GER"].Placed == false then
                 if cash >= placeUnits["Giorno_GER"].Cost then
-                    PlaceUnit("Giorno_GER",CFrame.new(-637.9058837890625, -57.04720687866211, -385.64453125))
+                    PlaceUnit("Giorno_GER",placeUnits["Giorno_GER"].Position)
                 end
             else
                 if placeUnits["ChaeHae"].Placed == false then
                     if cash >= placeUnits["ChaeHae"].Cost then
-                        PlaceUnit("ChaeHae",CFrame.new(-647.4136352539062, -57.04720687866211, -401.79046630859375))
+                        PlaceUnit("ChaeHae",placeUnits["ChaeHae"].Position)
                     end
                 else
                     if placeUnits["EscanorGodly"].Placed == false then
                         if cash >= placeUnits["EscanorGodly"].Cost then
-                            PlaceUnit("EscanorGodly",CFrame.new(-647.56640625, -57.04720687866211, -397.659423828125))
+                            PlaceUnit("EscanorGodly",placeUnits["EscanorGodly"].Position)
                         end
                     else
                         if placeUnits["DioOHShiny"].Placed == false then
                             if cash >= placeUnits["DioOHShiny"].Cost then
-                                PlaceUnit("DioOHShiny",CFrame.new(-641.1534423828125, -57.04720687866211, -399.4578552246094))
+                                PlaceUnit("DioOHShiny",placeUnits["DioOHShiny"].Position)
                             end
                         else
                             if placeUnits["Gogeta"].Placed == false then
                                 if cash >= placeUnits["Gogeta"].Cost then
-                                    PlaceUnit("Gogeta",CFrame.new(-643.4534912109375, -57.04720687866211, -387.2164306640625))
+                                    PlaceUnit("Gogeta",placeUnits["Gogeta"].Position)
                                 end
                             else
                                 allPlaced = true
